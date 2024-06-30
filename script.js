682cb05dd8281280d56f08aaf6f10ed2da5704c9
HaseefMuhammed <haseefmuhammedpc@gmail.com>
2024-06-30 21:29:09 +0530

    final file

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 function enterFullscreen() {
   const iframe = document.getElementById('fullscreen-iframe');
   
-  // Change the iframe source to the desired website
-  iframe.src = "https://haseefmuhammed.github.io/rhm-vote-server/start.html";
-
   // Attempt to enter full screen within the iframe
   if (iframe.requestFullscreen) {
-      iframe.requestFullscreen().then(hideButton);
-  } else if (iframe.mozRequestFullScreen) {
+      iframe.requestFullscreen().then(hideButton).catch(handleError);
+  } else if (iframe.mozRequestFullScreen) { // Firefox
       iframe.mozRequestFullScreen();
       hideButton();
-  } else if (iframe.webkitRequestFullscreen) {
-      iframe.webkitRequestFullscreen().then(hideButton);
-  } else if (iframe.msRequestFullscreen) {
+  } else if (iframe.webkitRequestFullscreen) { // Chrome, Safari, and Opera
+      iframe.webkitRequestFullscreen();
+      hideButton();
+  } else if (iframe.msRequestFullscreen) { // IE/Edge
       iframe.msRequestFullscreen();
       hideButton();
+  } else {
+      alert("Your browser does not support fullscreen API.");
   }
 }
 
@@ -22,4 +22,21 @@ function enterFullscreen() {
 function hideButton() {
   document.getElementById('fullscreen-btn').style.display = 'none';
   document.getElementById('iframe-container').style.display = 'block';
+}
+
+// Function to handle fullscreen errors
+function handleError(error) {
+  console.error("Error attempting to enable full-screen mode:", error);
+  alert("Failed to enter full-screen mode.");
+}
+
+function loginAdmin(){
+  let username = document.getElementById("inputUserName").value;
+  let password = document.getElementById("inputPassword").value;
+
+  if(username == "edubot" && password == "openapp.eb"){
+      window.location.href = "orgin.html";
+  }else{
+      alert("Invalid username or password");
+  }
 }
